2bc27f991bd28540d770c3d0f4718641399c4e5b
EmrahAKCAL <emrahakcl02@gmail.com>
2022-01-17 16:35:14 +0200

    mouseenter-event

---
+++ Jquery-Mouse-Enter-Olayı/mouseenter-event.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    //mouseenter() event use
+   $('.mouse-enter').mouseenter(function(){
+    $(this).addClass('highLight');
+    alert(`Hello Jquery Family!`);
+    });
+});
